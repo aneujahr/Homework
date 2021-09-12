3a01b3d3ee4b5f5cf8e6f86653b247eefe81587b
aneujahr <90078425+aneujahr@users.noreply.github.com>
2021-09-11 23:16:18 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+1. let ages = [3, 9, 23, 64, 2, 8, 28, 93];{
+    let removedelement= names.splice (0,7);
+        console.log(removed element);
+}
+    ages.unshift(13);
+    let ages = lengths.reduce(function(accumulator,currentvalue)){
+        return accumulator + currentvalue;
+        console.log(ages);
+    }
+
+
+2. let names = [ "Sam", "Tommy", "Tim", "Sally", "Buck", "Bob"];{
+    let sum = lengths.reduce(function(accumulator, currentvalue)){
+        return accumulator + currentvalue;
+    }
+}
+        console.log(sum);
+
+        names.forEach(function (element)) {
+            console.log(element);
+        };
+
+
+    3. console.log(names [5]);
+    4. console.log(names [0]);
+
+5. let nameLengths = [names];{
+    let lengths = names.map(function(element));{
+        console.log(lengths);
+    }
+        return element.lengths;
+}
+  
+    7.
+    8. function createFullName(firstName, lastName){
+        console.log(FirstName + "" + lastName);
+    }
+        createFullName("Allison", "Neujahr");
+       
+    9. function array(){
+        for let i = 0; i >= 100; i++){
+            console.log(i);
+        }  
+        return true;
+    } 
+    array();   
+
+    10. let sum = lengths.reduce(function(accumulator, currentvalue)){
+        return accumulator + currentvalue;
+    }
+        console.log(sum);
+
+    11. function array (array1, array2);{
+        let array2 = lengths.filter(function(element)){
+            return element.length % 2 == 0;
+        }
+            if (array1> array2) {
+                console.log(true);
+            }
+
+    }
+
+    12. fuction willbuydrink(isHotOutside, moneyInPocket);{
+        if (moneyInPocket > = $10.50){ return true}
+            else {return false}
+        }
+    }
+
+    
+    
+
+
+
+
+
+
